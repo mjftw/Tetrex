2a83c133f9e0f00c6ecc42e7a01f980e46db3e29
Merlin Webster <mjftwebster@gmail.com>
2022-07-29 21:18:49 +0100

    Can now move a Shape

--- lib/models/shape.ex
+++ lib/models/shape.ex
@@ -19,14 +19,14 @@ defmodule Tetrex.Shape do
       cols: 1
     }
   """
-  @spec new([[atom() | nil]]) :: Shape.t()
+  @spec new([[atom() | nil]]) :: __MODULE__.t()
   def new(squares_2d) do
     for {row, row_num} <- Stream.with_index(squares_2d),
         {square, col_num} when square != nil <- Stream.with_index(row),
         reduce: %__MODULE__{squares: %{}, rows: 0, cols: 0} do
       %__MODULE__{squares: squares, rows: rows} ->
         %__MODULE__{
-          squares: Map.merge(squares, %{{row_num, col_num} => square}),
+          squares: Map.put(squares, {row_num, col_num}, square),
           rows: max(rows, row_num),
           # Latest col_num is always the max so no need to check
           cols: col_num
@@ -34,10 +34,20 @@ defmodule Tetrex.Shape do
     end
   end
 
+  @spec move(__MODULE__.t(), {integer(), integer()}) :: __MODULE__.t()
+  def move(shape, {offset_rows, offset_cols}) do
+    squares =
+      shape.squares
+      |> Enum.map(fn {{row, col}, value} -> {{row + offset_rows, col + offset_cols}, value} end)
+      |> Map.new()
+
+    %__MODULE__{squares: squares, rows: shape.rows, cols: shape.cols}
+  end
+
   @doc """
   Combine two Shapes. In the case of overlaps values from the second shape overwrite the first.
   """
-  @spec merge(Shape.t(), Shape.t()) :: Shape.t()
+  @spec merge(__MODULE__.t(), __MODULE__.t()) :: __MODULE__.t()
   def merge(shape1, shape2) do
     %__MODULE__{
       squares: Map.merge(shape1.squares, shape2.squares),
--- test/models/shape_test.exs
+++ test/models/shape_test.exs
@@ -34,4 +34,32 @@ defmodule Tetrex.Shape.Test do
 
     assert merged == expected
   end
+
+  test "move/2 offsets all the coordinates in a Shape" do
+    shape = %Tetrex.Shape{
+      cols: 1,
+      rows: 1,
+      squares: %{
+        {0, 0} => :blue,
+        {0, 1} => :red,
+        {1, 0} => :red,
+        {1, 1} => :green
+      }
+    }
+
+    moved = Tetrex.Shape.move(shape, {-2, 1})
+
+    expected = %Tetrex.Shape{
+      cols: 1,
+      rows: 1,
+      squares: %{
+        {-2, 1} => :blue,
+        {-2, 2} => :red,
+        {-1, 1} => :red,
+        {-1, 2} => :green
+      }
+    }
+
+    assert moved == expected
+  end
 end
