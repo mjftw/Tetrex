f5bef2521ccba7d5c6c16e9867530c8c4b8c616a
Merlin Webster <mjftwebster@gmail.com>
2022-07-30 10:57:57 +0100

    Can create an empty game Board

---
+++ lib/models/board.ex
@@ -0,0 +1,37 @@
+defmodule Tetrex.Board do
+  alias Tetrex.SparseGrid
+  alias Tetrex.Tetromino
+
+  @tile_bag_size 99999
+
+  @enforce_keys [
+    :playfield,
+    :playfield_height,
+    :playfield_width,
+    :current_tile,
+    :hold_tile,
+    :upcoming_tiles
+  ]
+  defstruct [
+    :playfield,
+    :playfield_height,
+    :playfield_width,
+    :current_tile,
+    :hold_tile,
+    :upcoming_tiles
+  ]
+
+  @spec new(non_neg_integer(), non_neg_integer(), integer()) :: __MODULE__.t()
+  def new(width, height, random_seed) do
+    [current_tile | upcoming_tiles] = Tetromino.draw_randoms(@tile_bag_size, random_seed)
+
+    %__MODULE__{
+      playfield: SparseGrid.new(),
+      playfield_height: height,
+      playfield_width: width,
+      current_tile: current_tile,
+      hold_tile: nil,
+      upcoming_tiles: upcoming_tiles
+    }
+  end
+end
