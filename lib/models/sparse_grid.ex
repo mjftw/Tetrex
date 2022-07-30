17812bdd2455b0e1b5fb9025d1a15c367577716a
Merlin Webster <mjftwebster@gmail.com>
2022-07-30 07:24:41 +0100

    Refactor: variable renaming

--- lib/models/sparse_grid.ex
+++ lib/models/sparse_grid.ex
@@ -1,7 +1,7 @@
 defmodule Tetrex.SparseGrid do
   @moduledoc """
-  Data structure for holding a 2d grid of values.
-  Not every coordinate must have a value, in this sense it is a sparse grid.
+  Data structure for holding a 2d grid of grid.
+  Not every coordinate must have a grid, in this sense it is a sparse grid.
 
   E.g.
   ```
@@ -23,7 +23,7 @@ defmodule Tetrex.SparseGrid do
 
   @doc """
   Create a new SparseGrid from a 2d list of values.
-  To leave a value empty, `nil` can be used.
+  To leave a grid empty, `nil` can be used.
 
   E.g.
   ```
@@ -44,9 +44,9 @@ defmodule Tetrex.SparseGrid do
   end
 
   @spec move(sparse_grid(), coordinate()) :: sparse_grid()
-  def move(values, offset) do
-    values
-    |> move_values(offset)
+  def move(grid, offset) do
+    grid
+    |> move_grid(offset)
     |> Map.new()
   end
 
@@ -54,9 +54,9 @@ defmodule Tetrex.SparseGrid do
   Rotate the shape around the origin
   """
   @spec rotate(sparse_grid(), angle()) :: sparse_grid()
-  def rotate(values, angle) do
-    values
-    |> rotate_values(angle)
+  def rotate(grid, angle) do
+    grid
+    |> rotate_grid(angle)
     |> Map.new()
   end
 
@@ -64,30 +64,30 @@ defmodule Tetrex.SparseGrid do
   Rotate the shape around a specific point of rotation
   """
   @spec rotate(sparse_grid(), angle(), coordinate()) :: sparse_grid()
-  def rotate(values, angle, {rotate_at_x, rotate_at_y}) do
+  def rotate(grid, angle, {rotate_at_x, rotate_at_y}) do
     # Rotating around a point is the same as moving to the origin, rotating, and moving back
-    values
-    |> move_values({-rotate_at_x, -rotate_at_y})
-    |> rotate_values(angle)
-    |> move_values({rotate_at_x, rotate_at_y})
+    grid
+    |> move_grid({-rotate_at_x, -rotate_at_y})
+    |> rotate_grid(angle)
+    |> move_grid({rotate_at_x, rotate_at_y})
     |> Map.new()
   end
 
   @doc """
-  Combine two SparseGrids. In the case of overlaps values from the second shape overwrite the first.
+  Combine two SparseGrids. In the case of overlaps vales from the second grid overwrite the first.
   """
   @spec merge(sparse_grid(), sparse_grid()) :: sparse_grid()
-  def merge(values1, values2) do
-    Map.merge(values1, values2)
+  def merge(grid1, grid2) do
+    Map.merge(grid1, grid2)
   end
 
-  defp move_values(values, {x_offset, y_offset}) do
-    Stream.map(values, fn {{col, row}, value} -> {{col + x_offset, row + y_offset}, value} end)
+  defp move_grid(grid, {x_offset, y_offset}) do
+    Stream.map(grid, fn {{col, row}, grid} -> {{col + x_offset, row + y_offset}, grid} end)
   end
 
-  defp rotate_values(values, angle) do
-    Stream.map(values, fn {coordinate, value} ->
-      {rotate_coordinate(coordinate, angle), value}
+  defp rotate_grid(grid, angle) do
+    Stream.map(grid, fn {coordinate, grid} ->
+      {rotate_coordinate(coordinate, angle), grid}
     end)
   end
 
