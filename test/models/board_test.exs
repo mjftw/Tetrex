e62bc7f7511e84ca0b980d5e32c7527ba5b4bb7b
Merlin Webster <mjftwebster@gmail.com>
2022-08-11 16:39:08 +0100

    Fix bad flaky randomness tests

--- test/models/board_test.exs
+++ test/models/board_test.exs
@@ -3,29 +3,11 @@ defmodule Tetrex.Board.Test do
 
   alias Tetrex.Board
 
-  test "new/3 creates a specific board given a specific seed" do
-    board = Tetrex.Board.new(20, 10, 3_141_592_654)
+  test "new/3 creates a the same board given the same seed" do
+    board1 = Tetrex.Board.new(20, 10, 3_141_592_654)
+    board2 = Tetrex.Board.new(20, 10, 3_141_592_654)
 
-    upcoming = Enum.take(board.upcoming_tile_names, 10)
-
-    expected = %{
-      playfield: Tetrex.SparseGrid.new(),
-      playfield_height: 20,
-      playfield_width: 10,
-      active_tile: Tetrex.Tetromino.fetch!(:t),
-      next_tile: Tetrex.Tetromino.fetch!(:s),
-      hold_tile: nil
-    }
-
-    expected_upcoming = [:t, :l, :z, :o, :t, :o, :l, :l, :o, :z]
-
-    assert board.playfield == expected.playfield
-    assert board.playfield_height == expected.playfield_height
-    assert board.playfield_width == expected.playfield_width
-    assert board.active_tile == expected.active_tile
-    assert board.next_tile == expected.next_tile
-    assert board.hold_tile == expected.hold_tile
-    assert upcoming == expected_upcoming
+    assert board1 == board2
   end
 
   test "merge_active_draw_next/1 should change the current tile when no overlap would occur" do
