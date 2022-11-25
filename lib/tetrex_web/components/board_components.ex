3492ed876eeebb94d13a8f9456d9068750b53f68
Merlin Webster <merlin.webster@multiverse.io>
2022-11-25 18:12:56 +0000

    Tiles displaying on a grid, but not working Currently displaying in single column rather than 2d :-(

---
+++ lib/tetrex_web/components/board_components.ex
@@ -0,0 +1,33 @@
+defmodule TetrexWeb.Components.BoardComponents do
+  use TetrexWeb, :component
+  alias Tetrex.SparseGrid
+  alias Tetrex.Tetromino
+
+  def playfield(assigns) do
+    ~H"""
+      <div class="playfield">
+        <.sparsegrid_fixed sparsegrid={@board.playfield} width={@playfield_width} height={@playfield_height} />
+      </div>
+    """
+  end
+
+  defp sparsegrid_fixed(assigns) do
+    ~H"""
+      <div class="sparsegrid" style={"--num-rows: #{@height}, --num-columns: #{@width}"}>
+        <%= for x <- 0..@width, y <- 0..@height do %>
+          <div class={@sparsegrid |> SparseGrid.get(y, x) |> tile_class()}/>
+        <% end %>
+      </div>
+    """
+  end
+
+  defp tile_class(tile), do: "tetris-tile-" <> tile_class_suffix(tile)
+  defp tile_class_suffix(:red), do: "red"
+  defp tile_class_suffix(:green), do: "green"
+  defp tile_class_suffix(:blue), do: "blue"
+  defp tile_class_suffix(:cyan), do: "cyan"
+  defp tile_class_suffix(:yellow), do: "yellow"
+  defp tile_class_suffix(:purple), do: "purple"
+  defp tile_class_suffix(:grey), do: "grey"
+  defp tile_class_suffix(nil), do: "empty"
+end
--- lib/tetrex_web/live/board_live.ex
+++ lib/tetrex_web/live/board_live.ex
@@ -1,10 +1,21 @@
 defmodule TetrexWeb.BoardLive do
   use TetrexWeb, :live_view
 
-  alias TetrexWeb.Components.Board
+  alias TetrexWeb.Components.BoardComponents
 
   @impl true
   def mount(_params, _session, socket) do
+    # TODO: Define board outside and pass in - just for dev
+    board = Tetrex.Board.new(20, 10, 3)
+    preview = Tetrex.Board.preview(board)
+
+    # TODO: Add width and height to Board.preview
+    socket =
+      socket
+      |> assign(board: preview)
+      |> assign(playfield_width: board.playfield_width)
+      |> assign(playfield_height: board.playfield_height)
+
     {:ok, socket}
   end
 end
