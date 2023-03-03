7c887b125b1992e8bf64e4447e62588ee10aef0b
Merlin Webster <merlin.webster@multiverse.io>
2023-03-03 09:16:51 +0000

    Moved presence tracking to useful macro

---
+++ lib/tetrex_web/live/presence_user_tracking.ex
@@ -0,0 +1,68 @@
+defmodule TetrexWeb.PresenceUserTracking do
+  @moduledoc """
+    Use this module to automatically add user tracking via Phoenix Presence to your LiveView
+
+    Opts:
+      module: Your Phoenix Presence module,
+      topic: The topic to publish/subscribe to presence updates on,
+      socket_current_user_assign_key: The key in the socket assigns where the current user can be found,
+      socket_users_assign_key: The key in the socket assigns where the present users list should be stored
+  """
+  @spec __using__(any) :: {:__block__, [], [{:=, [], [...]} | {:__block__, [...], [...]}, ...]}
+  defmacro __using__(opts) do
+    quote location: :keep, bind_quoted: [opts: opts] do
+      presence_module = Keyword.fetch!(opts, :module)
+      presence_topic = Keyword.fetch!(opts, :topic)
+      presence_current_user_assign_key = Keyword.fetch!(opts, :socket_current_user_assign_key)
+      presence_users_assign_key = Keyword.fetch!(opts, :socket_users_assign_key)
+
+      def mount_presence_init(socket) do
+        if connected?(socket) do
+          {:ok, _} =
+            unquote(presence_module).track(
+              self(),
+              unquote(presence_topic),
+              socket.assigns[unquote(presence_current_user_assign_key)],
+              %{
+                joined_at: inspect(System.system_time(:second))
+              }
+            )
+
+          Phoenix.PubSub.subscribe(Tetrex.PubSub, unquote(presence_topic))
+        end
+
+        handle_presence_joins(socket, unquote(presence_module).list(unquote(presence_topic)))
+      end
+
+      @impl true
+      def handle_info(%Phoenix.Socket.Broadcast{event: "presence_diff", payload: diff}, socket) do
+        {
+          :noreply,
+          socket
+          |> handle_joins_leaves(diff.leaves)
+          |> handle_presence_joins(diff.joins)
+        }
+      end
+
+      defp handle_presence_joins(socket, joins) do
+        Enum.reduce(joins, socket, fn {user, %{metas: [meta | _]}}, socket ->
+          assign(
+            socket,
+            unquote(presence_users_assign_key),
+            Map.put(socket.assigns[unquote(presence_users_assign_key)], user, meta)
+          )
+        end)
+      end
+
+      defp handle_joins_leaves(socket, leaves) do
+        Enum.reduce(leaves, socket, fn {user, _}, socket ->
+          assign(
+            socket,
+            unquote(presence_users_assign_key),
+            Map.delete(socket.assigns[unquote(presence_users_assign_key)], user)
+          )
+        end)
+      end
+    end
+  end
+end
--- lib/tetrex_web/presence.ex
+++ lib/tetrex_web/presence.ex
@@ -2,17 +2,4 @@ defmodule TetrexWeb.Presence do
   use Phoenix.Presence,
     otp_app: :tetrex,
     pubsub_server: Tetrex.PubSub
-
-  def all_rooms, do: "room:*"
-
-  def channel_name(room_type), do: "room:#{room_type}"
-
-  def track_room(user_id, room_type, game_pid \\ nil) do
-    {:ok, _} =
-      track(self(), "room:#{room_type}", user_id, %{
-        joined_at: inspect(System.system_time(:second)),
-        room_type: room_type,
-        game_pid: game_pid
-      })
-  end
 end
