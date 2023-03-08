a4a60ef0b9bfe13ad1d8048343539b72a8e406fc
Merlin Webster <mjftwebster@gmail.com>
2023-03-08 22:13:42 +0000

    Added typespec to single player game

--- lib/tetrex/single_player/game.ex
+++ lib/tetrex/single_player/game.ex
@@ -1,4 +1,15 @@
 defmodule Tetrex.SinglePlayer.Game do
+  @type t :: %__MODULE__{
+          lines_cleared: non_neg_integer(),
+          board_pid: pid(),
+          periodic_mover_pid: pid(),
+          status:
+            :intro
+            | :playing
+            | :paused
+            | :game_over
+        }
+
   @enforce_keys [:lines_cleared, :board_pid, :periodic_mover_pid, :status]
   defstruct [:lines_cleared, :board_pid, :periodic_mover_pid, :status]
 end
