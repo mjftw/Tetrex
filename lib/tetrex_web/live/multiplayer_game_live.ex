5d210b6ad45c7de652f05005b809b4ce7ecc4613
Merlin Webster <merlin.webster@multiverse.io>
2023-03-17 16:25:08 +0000

    Prevent users from joining the same multiplayer game from multiple tabs

--- lib/tetrex_web/live/multiplayer_game_live.ex
+++ lib/tetrex_web/live/multiplayer_game_live.ex
@@ -1,4 +1,5 @@
 defmodule TetrexWeb.MultiplayerGameLive do
+  alias Tetrex.Multiplayer
   alias Tetrex.SinglePlayer.GameServer
   alias Tetrex.Multiplayer.GameMessage
   alias Tetrex.Multiplayer.GameServer
@@ -15,17 +16,30 @@ defmodule TetrexWeb.MultiplayerGameLive do
     case GameDynamicSupervisor.multiplayer_game_by_id(game_id) do
       # TODO: Log an error here
       {:error, _error} ->
-        {:noreply, push_redirect(socket, to: Routes.live_path(socket, TetrexWeb.LobbyLive))}
+        {:noreply, redirect_to_lobby(socket)}
 
-      {:ok, game_server, _game} ->
-        if connected?(socket) do
-          GameServer.subscribe_updates(game_server)
-          GameServer.join_game(game_server, user_id)
-          ProcessMonitor.monitor(fn _reason -> GameServer.leave_game(game_server, user_id) end)
+      {:ok, game_server_pid, game} ->
+        if Multiplayer.Game.user_in_game?(game, user_id) do
+          {:noreply,
+           socket
+           |> put_flash(
+             :error,
+             "Cannot join as you're already in the game. Is it open in another tab?"
+           )
+           |> redirect_to_lobby()}
+        else
+          if connected?(socket) do
+            GameServer.subscribe_updates(game_server_pid)
+            GameServer.join_game(game_server_pid, user_id)
+
+            ProcessMonitor.monitor(fn _reason ->
+              GameServer.leave_game(game_server_pid, user_id)
+            end)
+          end
+
+          initial_game_state = GameServer.get_game_message(game_server_pid)
+          {:noreply, assign(socket, game: initial_game_state, game_server_pid: game_server_pid)}
         end
-
-        initial_game_state = GameServer.get_game_message(game_server)
-        {:noreply, assign(socket, game: initial_game_state, game_server: game_server)}
     end
   end
 
@@ -33,4 +47,7 @@ defmodule TetrexWeb.MultiplayerGameLive do
   def handle_info(%GameMessage{} = game_state, socket) do
     {:noreply, assign(socket, game: game_state)}
   end
+
+  defp redirect_to_lobby(socket),
+    do: push_redirect(socket, to: Routes.live_path(socket, TetrexWeb.LobbyLive))
 end
