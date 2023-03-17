24c9ee05bfc1500d510596fe3879a15112048188
Merlin Webster <merlin.webster@multiverse.io>
2023-03-17 15:39:44 +0000

    Fix mix compilers warning

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Tetrex.MixProject do
       version: "0.4.2",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:gettext] ++ Mix.compilers(),
+      compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
