3d3ed1c652b68021da7485350ef6f738086bd2e2
Merlin Webster <merlin.webster@multiverse.io>
2023-06-01 13:19:00 +0100

    Fix docker entrypoint

--- docker_dev/entrypoint.sh
+++ docker_dev/entrypoint.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 # Docker entrypoint script.
 
-# Wait until Postgres is ready.
-while ! pg_isready -h postgres -p $PGPORT -U $PGUSER; do
-  echo "$(date) - Waiting for postgres to start..."
-  sleep 1
-done
-
 mix deps.get
-mix ecto.create
-mix ecto.migrate
 
 exec mix phx.server
