5b1379b83da2ff639c0f915fb88fe8d8774f75ee
Merlin Webster <mjftwebster@gmail.com>
2023-06-03 18:45:53 +0100

    Change MP game players to be a map of user to state. Also made updating score work

--- lib/tetrex/multiplayer/game_message.ex
+++ lib/tetrex/multiplayer/game_message.ex
@@ -2,17 +2,18 @@ defmodule Tetrex.Multiplayer.GameMessage do
   alias Tetrex.Multiplayer.Game
   alias Tetrex.Board
 
+  @type id :: String.t()
+
   @type t :: %__MODULE__{
           game_id: String.t(),
-          players: [
-            %{
-              user_id: String.t(),
+          players: %{
+            id() => %{
               board_preview: Board.board_preview(),
               lines_cleared: non_neg_integer(),
               status: Game.player_status(),
               online: boolean()
             }
-          ],
+          },
           status: Game.game_status()
         }
 
