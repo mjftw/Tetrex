6c6b3257c30a9fbf948201069215bfd7f1bf1f29
Merlin Webster <merlin.webster@multiverse.io>
2023-06-11 12:08:41 +0100

    Do not accept input after game finished

--- lib/tetrex_web/live/multiplayer_game_live.ex
+++ lib/tetrex_web/live/multiplayer_game_live.ex
@@ -64,6 +64,15 @@ defmodule TetrexWeb.MultiplayerGameLive do
   @impl true
   def handle_event(
         "keypress",
+        _,
+        %{assigns: %{game: %{status: :finished}}} = socket
+      ) do
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event(
+        "keypress",
         %{"key" => "ArrowDown"},
         %{assigns: %{user_id: user_id, game_server_pid: game_server_pid}} = socket
       ) do
