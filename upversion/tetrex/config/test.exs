32f98120081ec5d98ff6131fdf832863045e35c2
Merlin Webster <merlin.webster@multiverse.io>
2023-06-16 18:19:36 +0100

    added empty project with mix phx.new --no-ecto --no-mailer tetrex

---
+++ upversion/tetrex/config/test.exs
@@ -0,0 +1,14 @@
+import Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :tetrex, TetrexWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "Yt5vA09bckVmrvDJS7QEgwrsYaYjLGkaRxcYGZFyBEHa21JEdbongbNfjfubYCh2",
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
