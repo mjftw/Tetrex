474ee44f1b095e10304d17c81363ee26302fbe26
Merlin Webster <merlin.webster@multiverse.io>
2023-06-21 22:26:40 +0100

    Auto tailwind formatting

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,10 @@
 [
   import_deps: [:phoenix],
-  inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"],
+  plugins: [TailwindFormatter.MultiFormatter],
+  inputs: [
+    "*.{heex,ex,exs}",
+    "priv/*/seeds.exs",
+    "{config,lib,test}/**/*.{heex,ex,exs}"
+  ]
 ]
