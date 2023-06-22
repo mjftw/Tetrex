4155133cf0d52021e684b7d91014234d32e3d974
Merlin Webster <merlin.webster@multiverse.io>
2023-06-22 18:59:04 +0100

    Added tailwind styles for most of single player game

--- lib/tetrex_web/components/board_components.ex
+++ lib/tetrex_web/components/board_components.ex
@@ -1,46 +1,87 @@
 defmodule TetrexWeb.Components.BoardComponents do
-  use TetrexWeb, :live_component
+  use TetrexWeb, :html
   alias Tetrex.SparseGrid
-  alias TetrexWeb.Components.Board.SparseGridFixed
 
-  def playfield(assigns) do
+  @doc """
+  Board component. Assumes board size is 10x20.
+  Have to hard code this rather than passing as attrs as Tailwind CSS
+  cannot handle dynamic classes due to JIT compilation.
+  This means you cannot do "grid-cols-# {num_cols}".
+  """
+  attr(:sparsegrid, :map, required: true)
+
+  def board(assigns) do
     ~H"""
-    <div class="playfield">
-      <SparseGridFixed.render
-        sparsegrid={@board.playfield}
-        width={@board.playfield_width}
-        height={@board.playfield_height}
-      />
+    <div class="grid grid-cols-10">
+      <%= for y <- 0..19, x <- 0..9 do %>
+        <.tile type={SparseGrid.get(@sparsegrid, y, x)} />
+      <% end %>
     </div>
     """
   end
 
-  def score_box(assigns) do
+  @doc """
+  Single tile box component. Assumes tile needs 4x4 grid to fit
+  Have to hard code this rather than passing as attrs as Tailwind CSS
+  cannot handle dynamic classes due to JIT compilation.
+  This means you cannot do "grid-cols-# {num_cols}".
+  """
+  attr(:sparsegrid, :map, required: true)
+
+  def single_tile(assigns) do
     ~H"""
-    <div class="score-box">
-      Score: <%= @score %>
+    <% sparsegrid = centre_single_tile(@sparsegrid || SparseGrid.empty()) %>
+    <div class="grid grid-cols-4">
+      <%= for y <- 0..3, x <- 0..3 do %>
+        <.tile type={SparseGrid.get(sparsegrid, y, x)} />
+      <% end %>
     </div>
     """
   end
+
+  attr(:title, :string, required: true)
+  attr(:sparsegrid, :map, required: true)
+
+  def single_tile_box(assigns) do
+    ~H"""
+    <div class={"#{box_default_styles()} p-2"}>
+      <%= @title %> <.single_tile sparsegrid={@sparsegrid} />
+    </div>
+    """
+  end
+
+  attr(:board, :map, required: true)
 
   def next_tile_box(assigns) do
     ~H"""
-    <div class="tile-box">
-      Next
-      <SparseGridFixed.render sparsegrid={centre_single_tile(@board.next_tile)} width={4} height={4} />
+    <.single_tile_box title="Next" sparsegrid={@board.next_tile} />
+    """
+  end
+
+  attr(:board, :map, required: true)
+
+  def hold_tile_box(assigns) do
+    ~H"""
+    <.single_tile_box title="Hold" sparsegrid={@board.hold_tile} />
+    """
+  end
+
+  attr(:board, :map, required: true)
+
+  def playfield(assigns) do
+    ~H"""
+    <div class={box_default_styles()}>
+      <.board sparsegrid={@board.playfield} />
     </div>
     """
   end
 
-  def hold_tile_box(assigns) do
+  attr(:score, :integer, required: true)
+
+  def score_box(assigns) do
     ~H"""
-    <div class="tile-box">
-      Hold
-      <SparseGridFixed.render
-        sparsegrid={centre_single_tile(@board.hold_tile || SparseGrid.empty())}
-        width={4}
-        height={4}
-      />
+    <div class={"#{box_default_styles()} p-2 text-xl"}>
+      Score: <%= @score %>
     </div>
     """
   end
@@ -57,4 +98,70 @@ defmodule TetrexWeb.Components.BoardComponents do
   end
 
   defp centre_single_tile(tile), do: SparseGrid.align(tile, :centre, {0, 0}, {3, 3})
+
+  defp box_default_styles,
+    do:
+      "m-1 h-fit w-fit rounded-md border-2 border-solid border-slate-700 bg-orange-100 text-center"
+
+  attr(:type, :atom, required: true)
+
+  defp tile(%{type: :red} = assigns) do
+    ~H"""
+    <div class="h-7 w-7 bg-red-400" />
+    """
+  end
+
+  defp tile(%{type: :green} = assigns) do
+    ~H"""
+    <div class="h-7 w-7 bg-green-400" />
+    """
+  end
+
+  defp tile(%{type: :blue} = assigns) do
+    ~H"""
+    <div class="h-7 w-7 bg-blue-400" />
+    """
+  end
+
+  defp tile(%{type: :cyan} = assigns) do
+    ~H"""
+    <div class="h-7 w-7 bg-cyan-400" />
+    """
+  end
+
+  defp tile(%{type: :yellow} = assigns) do
+    ~H"""
+    <div class="h-7 w-7 bg-yellow-400" />
+    """
+  end
+
+  defp tile(%{type: :purple} = assigns) do
+    ~H"""
+    <div class="h-7 w-7 bg-purple-400" />
+    """
+  end
+
+  defp tile(%{type: :orange} = assigns) do
+    ~H"""
+    <div class="h-7 w-7 bg-orange-400" />
+    """
+  end
+
+  defp tile(%{type: :drop_preview} = assigns) do
+    ~H"""
+    <div class="h-7 w-7 border-2 border-solid border-slate-200 bg-none" />
+    """
+  end
+
+  defp tile(%{type: :blocking} = assigns) do
+    ~H"""
+    <div class="border-1 h-7 w-7 border-solid border-slate-800 bg-slate-700" />
+    """
+  end
+
+  defp tile(%{type: nil} = assigns) do
+    ~H"""
+    <div class="h-7 w-7 bg-none" />
+    """
+  end
 end
