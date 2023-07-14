41d2bdcce929d2b6b8e8336d32e5b26312f80687
Merlin Webster <merlin.webster@multiverse.io>
2023-07-14 15:47:34 +0100

    Added a mute button - layout needs work

--- lib/tetrex_web/components/client/audio.ex
+++ lib/tetrex_web/components/client/audio.ex
@@ -1,12 +1,51 @@
 defmodule TetrexWeb.Components.Client.Audio do
   alias TetrexWeb.Components.Soundtrack
+
   use TetrexWeb, :live_component
 
-  def tetris_audio(assigns) do
+  @impl true
+  def mount(socket) do
+    {:ok, socket |> assign(muted: false)}
+  end
+
+  attr :muted, :boolean, required: true
+  attr :id, :string, required: true
+  attr :class, :string, default: nil
+
+  @impl true
+  def render(assigns) do
     ~H"""
-    <Soundtrack.background id={theme_music_audio_id()} src={theme_music_audio_src()} loop />
-    <Soundtrack.effect id={game_over_audio_id()} src={game_over_audio_src()} />
+    <div class={@class}>
+      <Soundtrack.background id={theme_music_audio_id()} src={theme_music_audio_src()} />
+      <Soundtrack.effect id={game_over_audio_id()} src={game_over_audio_src()} />
+
+      <div phx-click="toggle-muted" phx-target={@myself}>
+        <%= if @muted do %>
+          <svg class="hero-speaker-x-mark" />
+        <% else %>
+          <svg class="hero-speaker-wave" />
+        <% end %>
+      </div>
+    </div>
     """
+  end
+
+  @impl true
+  def handle_event("toggle-muted", _, socket) do
+    socket =
+      if socket.assigns.muted do
+        socket
+        |> assign(:muted, false)
+        |> push_event("unmute-audio", %{id: theme_music_audio_id()})
+        |> push_event("unmute-audio", %{id: game_over_audio_id()})
+      else
+        socket
+        |> assign(:muted, true)
+        |> push_event("mute-audio", %{id: theme_music_audio_id()})
+        |> push_event("mute-audio", %{id: game_over_audio_id()})
+      end
+
+    {:noreply, socket}
   end
 
   def play_game_over_audio(socket),
--- lib/tetrex_web/components/soundtrack.ex
+++ lib/tetrex_web/components/soundtrack.ex
@@ -1,17 +1,23 @@
 defmodule TetrexWeb.Components.Soundtrack do
   use TetrexWeb, :live_component
 
+  attr :id, :string, required: true
+  attr :src, :string, required: true
+
   def background(assigns) do
     ~H"""
-    <audio id={@id} muted loop>
+    <audio id={@id} loop>
       <source src={@src} type="audio/mpeg" />
     </audio>
     """
   end
 
+  attr :id, :string, required: true
+  attr :src, :string, required: true
+
   def effect(assigns) do
     ~H"""
-    <audio id={@id} muted>
+    <audio id={@id}>
       <source src={@src} type="audio/mpeg" />
     </audio>
     """
