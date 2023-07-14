0d814717cd38aef6f85e3400ba0ffdf6f419b7b6
Merlin Webster <merlin.webster@multiverse.io>
2023-07-14 15:47:34 +0100

    Added a mute button - layout needs work

--- assets/js/audioEventListeners.js
+++ assets/js/audioEventListeners.js
@@ -1,7 +1,6 @@
 // Play an audio element on demand
 window.addEventListener("phx:play-audio", e => {
     let audio = document.getElementById(e.detail.id)
-    audio.muted = false
     audio.play()
 })
 
@@ -16,4 +15,17 @@ window.addEventListener("phx:stop-audio", e => {
 window.addEventListener("phx:pause-audio", e => {
     let audio = document.getElementById(e.detail.id)
     audio.pause()
+})
+
+// Mute audio source on demand
+window.addEventListener("phx:mute-audio", e => {
+    let audioElements = document.getElementsByTagName("audio")
+    let audio = document.getElementById(e.detail.id)
+    audio.muted = true
+})
+
+// Unmute audio source on demand
+window.addEventListener("phx:unmute-audio", e => {
+    let audio = document.getElementById(e.detail.id)
+    audio.muted = false
 })
