3805dea7b6a0569d3e9460814c3444363ba9a125
jersearls <searls@me.com>
2023-11-02 14:01:59 -0400

    working version, deploy with fly deploy

--- config/runtime.exs
+++ config/runtime.exs
@@ -32,16 +32,22 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :cars_commerce_tetris, CarsCommerceTetrisWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
-    http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
-    ],
+    http: [ip: {0, 0, 0, 0, 0, 0, 0, 0}, port: System.get_env("PORT") || 4000, compress: true],
+    url: [host: "localhost", port: System.get_env("PORT") || 4000],
+    check_origin: ["//*.fly.dev", "//localhost"],
+    cache_static_manifest: "priv/static/cache_manifest.json",
+    root: ".",
     secret_key_base: secret_key_base
+
+  # url: [host: host, port: 443, scheme: "https"],
+  # http: [
+  #   # Enable IPv6 and bind on all interfaces.
+  #   # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+  #   # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
+  #   # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+  #   ip: {0, 0, 0, 0, 0, 0, 0, 0},
+  #   port: port
+  # ],
 
   # ## SSL Support
   #
