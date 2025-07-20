1fb701c86c998857ce4cda073e9cc3abfcd2bc2c
Merlin Webster <mjftwebster@gmail.com>
2025-07-20 11:30:32 +0100

    Updated version to v0.24.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Tetrex.MixProject do
   def project do
     [
       app: :tetrex,
-      version: "0.23.0",
+      version: "0.24.0",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
