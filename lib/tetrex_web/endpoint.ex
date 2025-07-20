556838e1523b65aff6597d56d268a329ac8ce955
Merlin Webster <mjftwebster@gmail.com>
2025-07-20 09:25:07 +0100

    Added TideWave MCP server

--- lib/tetrex_web/endpoint.ex
+++ lib/tetrex_web/endpoint.ex
@@ -24,12 +24,17 @@ defmodule TetrexWeb.Endpoint do
     gzip: false,
     only: TetrexWeb.static_paths()
 
+  if Code.ensure_loaded?(Tidewave) do
+    plug Tidewave
+  end
+
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
     socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
     plug Phoenix.LiveReloader
     plug Phoenix.CodeReloader
+
   end
 
   plug Phoenix.LiveDashboard.RequestLogger,
